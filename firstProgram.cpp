533b25bc1607518f0e2c4e9735af2d9a25ee0ee2
CHiranjeebnayak.37@gmail.com <chiranjeebnayak.37@gmail.com>
2024-01-02 10:50:43 +0530

    Introduction to C++

---
+++ firstProgram.cpp
@@ -0,0 +1,9 @@
+#include<iostream> // header line
+using namespace std; // use varaiable and objects from standard library
+
+int main(){ // main function 
+    //body
+    cout<<"Hello World\t";// cout stands for print / output in console
+    cout<<"Codemap";
+}
+// comments 
