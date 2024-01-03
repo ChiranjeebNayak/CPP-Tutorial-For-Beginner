5499d3d921565dfeea00f8c143a01d8a15c3a571
CHiranjeebnayak.37@gmail.com <chiranjeebnayak.37@gmail.com>
2024-01-03 12:26:17 +0530

    Variables and DataTypes

---
+++ Variable.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){ 
+    //body
+    //varaiable declaration 
+    int x;
+    //variable initialization
+    x = 1;
+    cout<<"X value = "<<x<<endl;
+    //declare&&initilization
+    int y = 2;
+    y = x;
+    cout<<"Y value = "<<y<<endl;
+    float fl = 2.33;
+    cout<<"fl Value = "<<fl<<endl;
+    double dl = 5.8886;
+    cout<<"double value = "<<dl<<endl;
+    char ch = 'a';
+    cout<<"ch value = "<<ch<<endl;
+}
