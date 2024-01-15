790c6efb0200b18e349bd15f815e0f578883fc44
CHiranjeebnayak.37@gmail.com <chiranjeebnayak.37@gmail.com>
2024-01-15 16:39:13 +0530

    User Input and oprators

---
+++ operators.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a =2,b=3;
+    int c = a+b;
+    cout<<"Sum of a and b = "<<c<<endl;
+    // user input 
+    int x,y;
+    cout<<"enter x and y value  = ";
+    cin>>x>>y;
+    cout<<"x = "<<x<<" y = "<<y<<endl;
+    int z = x+y;
+    cout<<"sum of z = "<<z<<endl;
+    int s = x-y;
+    cout<<"sub of x and y = "<<abs(s)<<endl;
+    int m = x%y;
+    cout<<"modulo of x and y = "<<m<<endl;
+    // post increment x++; it will not increase value instantly 
+    cout<<"x value1 = "<<x++<<endl; //x = x+1
+    cout<<"x value2 = "<<x<<endl;
+    // pre increment ++x ; it will increase by 1 instantly
+    cout<<"y value1 = "<<++y<<endl;
+    cout<<"y value2 = "<<y<<endl;
+
+    // decremnt decrease by one 
+    // predec =  --x and postdec = x--;
+    cout<<"post dec x value1 = "<<x--<<endl;
+    cout<<"post dec x value2 = "<<x<<endl;
+    cout<<"pre dec x value1 = "<<--x<<endl;
+    cout<<"pre dec x value2 = "<<x<<endl;
+
+
+}
---
+++ userInput.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // user input cin 
+    int a =3;
+    string str;
+    cout<<"manual a value = "<<a<<endl;
+    int b;
+    cout<<"Enter input for b : ";
+    cin>>b;
+    cout<<"userInput b ="<<b<<endl;
+    cout<<"Enter input for str : ";
+    cin>>str;
+    cout<<"userInput str ="<<str<<endl;
+}
