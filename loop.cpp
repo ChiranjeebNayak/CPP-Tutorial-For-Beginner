5933c33529e360ace935babc2e257095cc0131b1
CHiranjeebnayak.37@gmail.com <chiranjeebnayak.37@gmail.com>
2024-04-22 01:06:11 +0530

    Loops in c++

---
+++ loop.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    // int count = 1; // initialize
+    // while(count<=5){ // in  while loop we put condition 
+    //     cout<<count<<" - "<<"Hello World"<<endl;
+    //     count++; //update count
+    // }
+    // cout<<"We are done";
+
+    // int count =1;
+    // while(count<=10){
+    //     cout<<count<<endl;
+    //     count++;
+    // }
+
+    // for(int count = 1;count<=5;count++){
+    //     cout<<count <<" - "<<"Hello World"<<endl;
+    // }
+    // for(int count = 1;count<=10;count++){
+    //     if(count%2 == 0){
+    //         cout<<count<<endl;
+    //     }
+    // }
+    // int count =1;
+    // do{
+    //     cout<<count<<" -"<<"Hello World"<<endl;
+    //     count++;
+    // }while(count<=5);
+    int count =1;
+    do{
+        cout<<count<<endl;
+        count++;
+    }while(count<=10);
+    cout<<"We are done";
+    return 0;
+}
